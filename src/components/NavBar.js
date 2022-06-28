2d3730fe6554c7f88ed2f968a8c9a964cc1d6b3f
lautylambach <lautylambach98@outlook.com.ar>
2022-06-28 00:21:49 -0300

    cambios en el NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,50 @@
-import React from 'react'
+import logo from '../logo.svg';
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import './NavBar.css'
+
 
 export const NavBar = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+
+    <div>
+    <Navbar className="nav-bar" bg="dark" variant={"dark"} expand="lg">
+      <Container fluid>
+      <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={logo}
+              width="50"
+              height="50"
+              className="d-inline-block align-top"
+            />{' '}
+            <span>Lambach Tienda Informatica</span>
+          </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link className='navlink' href="#inicio">Inicio</Nav.Link>
+            <Nav.Link className='navlink' href="#periferico">Perifericos</Nav.Link>
+            <Nav.Link className='navlink' href="#monitores">Monitores</Nav.Link>
+            <Nav.Link className='navlink' href="#placasmadres">PlacasMadres</Nav.Link>
+        
           </Nav>
+          <Form className="d-flex">
+            <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+            />
+            <Button className='btn' variant="light">Search</Button>
+            </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    </div>
+    
   )
 }
