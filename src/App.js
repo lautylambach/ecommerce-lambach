0e2ba9bf67860cd85260818772cbf87e635b805b
lautylambach <lautylambach98@outlook.com.ar>
2022-06-29 18:14:10 -0300

    app-js cambio menor

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     
     <div className="App">
-      <NavBar></NavBar>
+      <NavBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
