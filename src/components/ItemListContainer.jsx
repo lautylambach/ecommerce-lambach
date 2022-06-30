2e6cb79a6d9e851fd73e0d23a7d0d0f58c24d4a2
lautylambach <lautylambach98@outlook.com.ar>
2022-06-30 19:45:28 -0300

    cambios media

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -4,7 +4,7 @@ import './ItemListContainer.css'
 export const ItemListContainer =({greeting}) => {
     return(
         <>
-        <center>
+        
         <div className='greeting-banner'>
             <h3 className='greeting-h3'>Hola {greeting}</h3>
             <hr className='greeting-hr' />
@@ -12,7 +12,7 @@ export const ItemListContainer =({greeting}) => {
                 Listo para realizar una compra?!
             </p>
         </div>
-        </center>
+        
         </>
     )
 }
