c6ca7428218500dd99d8b3566fd76d1f239b0476
lautylambach <lautylambach98@outlook.com.ar>
2022-07-07 00:17:04 -0300

    ItemList

---
+++ src/components/Item.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../css/Item.css'
+export const Item = ({info}) => {
+  return (
+    <div className='item-container'>
+        <img className='item-img' src={info.imagen} alt={info.titulo} />
+        <h3 className='item-titulo'>{info.titulo}</h3>
+        <p className='item-desc'>{info.descripcion}</p>
+        <span className='item-precio'>{info.precio}</span>
+    </div>
+  )
+}
+export default Item;
---
+++ src/components/ItemList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Item from './Item';
+
+export const ItemList = ({data=[]}) => {
+  return (
+    data.map(producto => <Item key={producto.id} info={producto}/>)
+  )
+}
+export default ItemList;
