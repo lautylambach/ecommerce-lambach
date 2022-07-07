73b084dd037d5062c92af0f6de3bb30d35f1ca58
lautylambach <lautylambach98@outlook.com.ar>
2022-07-07 15:22:01 -0300

    solve error img

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,7 +3,7 @@ import '../css/Item.css'
 export const Item = ({info}) => {
   return (
     <div className='item-container'>
-        <img className='item-img' src={info.imagen} alt={info.titulo} />
+        <img className='item-img' src={info.image} alt={info.titulo} />
         <h3 className='item-titulo'>{info.titulo}</h3>
         <p className='item-desc'>{info.descripcion}</p>
         <span className='item-precio'>{info.precio}</span>
