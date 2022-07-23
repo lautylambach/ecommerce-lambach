19fe0cb526bfd8fc56949aeac5e13bc16784ec46
lautylambach <lautylambach98@outlook.com.ar>
2022-07-22 23:33:42 -0300

    ItemDetail y itemCount sincronizados

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import { Container } from 'react-bootstrap';
 import ItemCount from './ItemCount';
 import '../css/ItemDetail.css';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 export const ItemDetail = ({data}) => {
+    const[goToCart,setGoToCart] =useState(false);
+
 
     const onAdd = (quantity) =>{
-        console.log(`compraste ${quantity} unidades`);
+        console.log(`añadiste ${quantity} unidades`);
         console.log('se enviara al carrito')
+        setGoToCart(true);
         
     }
 
@@ -18,7 +23,17 @@ export const ItemDetail = ({data}) => {
                 <h2 className='item-titulo'> {data.titulo}</h2>
                 <p className='item-desc'>{data.descripcion}</p>
                 <span className='item-precio'>{data.precio}</span>
+                {
+                goToCart ? 
+                <>
                 <ItemCount initial={1} stockItem={data.stock} onAdd={onAdd}/>
+                <p>Añadiste elementos al carrito</p>
+                <Link to='/Cart'>Terminar Compra</Link> 
+                </>
+                : 
+                <ItemCount initial={1} stockItem={data.stock} onAdd={onAdd}/>
+                }
+                
             </div>
             
         </div>
