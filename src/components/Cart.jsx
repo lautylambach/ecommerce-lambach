4a741b47242fb878989f40398c889993e094743e
lautylambach <lautylambach98@outlook.com.ar>
2022-08-02 10:24:40 -0300

    itemCollection II

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,11 +2,31 @@ import React from 'react';
 import { useCartContext } from './CartContext';
 import {Link} from 'react-router-dom'
 import ItemCart from './ItemCart';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 
 const  Cart= ()=> {
   const {cart, totalPrice,clear} = useCartContext();
   console.log(cart.length)
-  
+  const order = {
+    buyer:{
+      nombre:'Lautaro',
+      email: 'lautaro@gmail.com',
+      numero: '123456789',
+      direccion: 'los tilos 73'
+    },
+    date: Date(),
+    items:cart.map(product =>({id: product.id, titulo: product.titulo, precio: product.precio, quantity: product.quantity})),
+    total: totalPrice(),
+  }
+  const handleClick = () =>{
+    const db = getFirestore();
+    const ordersCollection = collection(db,'orders');
+    addDoc(ordersCollection, order)
+    .then (({id}) => console.log(id))
+  }
+
+
+
   if (cart.length === 0){
     return(
       <>
@@ -22,6 +42,7 @@ const  Cart= ()=> {
         }
         <p>Precio Total: ${totalPrice()}</p>
         <button onClick={()=> clear()}>Borrar Carrito</button>
+        <button onClick={handleClick}>Generar orden</button>
       </>
     )
   
