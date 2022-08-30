583d69336b2c2e445e8c6e348c99b4d3a6d1c8c0
lautylambach <lautylambach98@outlook.com.ar>
2022-08-30 09:35:57 -0300

    feedback2

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,12 +3,10 @@ import { Button } from 'react-bootstrap'
 import '../css/Item.css'
 import {Link} from 'react-router-dom'
 
-
 export const Item = ({info}) => {
 
   return (
     <div className='item-container container-fluid'>
-      
       <div>
         <img className='item-img' src={info.image} alt={info.titulo} />
         <h3 className='item-titulo-list'>{info.titulo}</h3>
@@ -18,7 +16,6 @@ export const Item = ({info}) => {
         <Button className='btn-item' variant="primary" ><Link className='navlink' to={`/item/${info.id}`}>Comprar</Link></Button>
         
       </div>
-      
     </div>
   )
 }
